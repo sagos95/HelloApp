2c89ca8f4519407dc4078bdfec42453b1bd9a5c6
DOC <DOC@DESKTOP-PSL15DI>
2023-01-13 15:00:37 +0300

    Добавьте файлы проекта.

---
+++ HelloApp/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.DataAnnotations;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int firstNumber, secondNumber;
+        Console.WriteLine("Введите первое число - ");
+
+        firstNumber = Convert.ToString (Console.ReadLine());
+
+        Console.WriteLine("Введите второе число - ");
+
+        secondNumber = Convert.ToString(Console.ReadLine());
+
+        string simvol;
+        Console.WriteLine("Выберите действие: '+' '-' '*' '/');
+            simvol = Console.ReadLine();
+
+        for (simvol == "+")
+            {
+            Console,WriteLine(firstNumber + secondNumber);
+
+        }
+
+        else if (simvol == "-")
+        {
+            Console,WriteLine(firstNumber - secondNumber);
+
+        }
+        else if
+        {
+            simvol = '-';
+            Console.WriteLine(firstNumber - secondNumber);
+
+        }
+
+
+
+
+    }
+}
+
+    }
+}   
